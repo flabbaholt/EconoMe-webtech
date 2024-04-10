e41a7416ad6053df111aa769cf4cba2069de64b3
Louis Meyer-Breitländer <119730231+flabbaholt@users.noreply.github.com>
2024-04-10 20:34:39 +0200

    Initial Spring Setup

---
+++ src/test/java/com/econome/EconomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
