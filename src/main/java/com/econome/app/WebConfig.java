16d917a602e88023163da9394325eaceec25fb7c
Louis Meyer-Breitländer <119730231+flabbaholt@users.noreply.github.com>
2024-05-22 10:42:02 +0200

    added webconfig to allow frontend connection

---
+++ src/main/java/com/econome/app/WebConfig.java
@@ -0,0 +1,20 @@
+package com.econome.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:3000",
+                                "http://localhost:5173",
+                                "https://econome-webtech-frontend.onrender.com");
+    }
+}
