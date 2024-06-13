40b1abf004fd12b8f1c394b7354b3b4221b691d8
Louis Meyer-Breitländer <119730231+flabbaholt@users.noreply.github.com>
2024-06-13 20:42:33 +0200

    added comments

--- src/main/java/com/econome/app/repository/TransactionRepository.java
+++ src/main/java/com/econome/app/repository/TransactionRepository.java
@@ -9,9 +9,11 @@ import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
+    // This query is used to get all transactions with their respective category, payment method, type and currency
     @Query("SELECT t.id as id, t.name as name, t.amount as amount, t.transactionDate as transactionDate, c.name as categoryName, p.name as paymentMethodName, ty.name as typeName, cu.name as currencyName FROM Transaction t JOIN t.category c JOIN t.paymentMethod p JOIN t.type ty JOIN t.currency cu")
     List<TransactionProjection> findAllProjectedBy();
 
+    // This query is used to get the total balance of a specific month and year
     @Query("SELECT SUM(t.amount) FROM Transaction t WHERE YEAR(t.transactionDate) = ?1 AND MONTH(t.transactionDate) = ?2")
     Double getTotalBalance(Integer year, Integer month);
 }
